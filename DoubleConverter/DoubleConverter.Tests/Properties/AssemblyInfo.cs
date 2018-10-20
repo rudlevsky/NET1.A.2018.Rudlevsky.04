2227f33becd53efb2c3d8b8c7198dde00593d860
rudlevsky <rudlevsky@gmail.com>
2018-10-20 15:58:33 +0300

    second task added

---
+++ DoubleConverter/DoubleConverter.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DoubleConverter.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DoubleConverter.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dbdfb652-dcd9-47eb-a1e4-8ef2e99a4095")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DoubleConverter/DoubleConverter/Converter.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace DoubleConverter
+{
+    /// <summary>
+    /// Class extends type System.Double.
+    /// </summary>
+    public static class Converter
+    {
+        private const int BitsLongCount = 64;
+
+        /// <summary>
+        /// Extend functionality of type System.Double.
+        /// </summary>
+        /// <param name="number">Number which will be represented in IEEE format.</param>
+        /// <returns>String representation in IEEE format.</returns>
+        public static string ConvertToBinary(this double number) =>
+            new DoubleStruct(number).ConvertLong().ConvertToIEEE();
+
+        private static string ConvertToIEEE(this long bits)
+        {
+            var result = new StringBuilder(); // Set char[] and pass it to this constructor
+            for (int i = 0; i < BitsLongCount; i++)
+            {
+                if ((bits & 1) == 1)
+                {
+                    result.Insert(0, "1");
+                }
+                else
+                {
+                    result.Insert(0, "0");
+                }
+
+                bits >>= 1;
+            }
+
+            return result.ToString();
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct DoubleStruct
+        {
+            [FieldOffset(0)]
+            private double dbBits;
+
+            [FieldOffset(0)]
+            private long lgBits;
+
+            public DoubleStruct(double variable) : this()
+            {
+                this.dbBits = variable;
+            }
+
+            public long ConvertLong() => this.lgBits;
+        }
+    }
+}

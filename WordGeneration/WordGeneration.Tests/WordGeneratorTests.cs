87e22fdbe3b2088851e2f8ef5c1ae541beca9010
rudlevsky <rudlevsky@gmail.com>
2018-10-23 10:20:10 +0300

    tests added

--- WordGeneration/WordGeneration.Tests/WordGeneratorTests.cs
+++ WordGeneration/WordGeneration.Tests/WordGeneratorTests.cs
@@ -11,8 +11,15 @@ namespace WordGeneration.Tests
         {"minus two point five four", "zero point three five", "one point seven"})]
         [TestCase(new double[] { 0.378, 6685.7 },ExpectedResult = new string[]
         {"zero point three seven eight", "six six eight five point seven"})]
-        public string[] TransformToWordsTest_WithCorrectArrays(double[] doubles)
+        public string[] TransformToWordsTest_WithCorrectArrays_CorrectRusult(double[] doubles)
         => WordGenerator.TransformToWords(doubles);
+
+        [TestCase(new double[] { -255.255, 255.255, 4294967295.0 }, ExpectedResult = new string[]
+        {"1100000001101111111010000010100011110101110000101000111101011100",
+         "0100000001101111111010000010100011110101110000101000111101011100",
+         "0100000111101111111111111111111111111111111000000000000000000000" })]
+        public string[] TransformToIEEEFormat_WithCorrectArrays_CorrectResult(double[] doubles)
+        => WordGenerator.TransformToIEEEFormat(doubles);
 
         [Test]
         public void TransformToWords_NullReference_ArgumentNullException()
